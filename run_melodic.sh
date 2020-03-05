d38f53a14399f062ea53fff75957a041b6fd0ad8
Jenna <jmschabdach@gmail.com>
2020-03-05 10:34:01 -0500

    removed accidental file

--- run_melodic.sh
+++
@@ -1,4 +0,0 @@
-rm -rf ./testing_melodic/
-melodic -i sandbox/pseudo_BOLD.nii.gz --report -o testing_melodic --Oall
-fslcc --noabs -p 3 -t 0.005 sandbox/dmn_roi.nii.gz testing_melodic/melodic_IC.nii.gz
-
